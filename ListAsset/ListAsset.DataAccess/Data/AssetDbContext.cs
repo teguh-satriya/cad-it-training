a88f0bb632e5a6aa5c1f8f1bc88f8c0933097db7
Teguh Satriya <teguh.satriya.w@gmail.com>
2023-02-09 10:42:46 +0700

    apply three tier architecture

---
+++ ListAsset/ListAsset.DataAccess/Data/AssetDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ListAsset.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListAsset.DataAccess.Data
+{
+    public class AssetDbContext:DbContext
+    {
+        public AssetDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Asset> Assets { get; set; }
+        DbSet<Country> Countries { get; set; }
+    }
+}
---
+++ ListAsset/ListAsset.DataAccess/Models/Asset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAsset.DataAccess.Models
+{
+    public class Asset
+    {
+        public int AssetId { get; set; }
+        public string AssetCode { get; set; }
+        public string AssetName { get; set; }
+        public string AssetType { get; set; }
+        public int Quantity { get; set; }
+
+        public int CountryId { get; set; }
+        public virtual Country Country { get; set; }
+    }
+}
