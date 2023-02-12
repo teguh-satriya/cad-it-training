cf200f31d88e5ea1be5d2850f006817983c1a748
Teguh Satriya <teguh.satriya.w@gmail.com>
2023-02-13 03:14:59 +0700

    fixing

---
+++ ListAsset/ListAsset.BusinessAccess/Services/CountryService.cs
@@ -0,0 +1,32 @@
+﻿using ListAsset.DataAccess.Contracts;
+using ListAsset.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAsset.BusinessAccess.Services
+{
+    public class CountryService
+    {
+        public readonly IRepository<Country> _repository;
+        public CountryService(IRepository<Country> repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Country> GetAllUser()
+        {
+            try
+            {
+                return _repository.GetAll().ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
